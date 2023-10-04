de2fa794353f0a67066e8d86755720823d65a0b8
Sharvari4179 <84309046+Sharvari4179@users.noreply.github.com>
2023-10-04 15:13:08 +0530

    Add files via upload

---
+++ client/src/Features/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeSliceReducer from "./themeSlice";
+import refreshSidebar from "./refreshSidebar";
+
+export const store = configureStore({
+  reducer: {
+    themeKey: themeSliceReducer,
+    refreshKey: refreshSidebar,  },
+});
